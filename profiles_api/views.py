762d794fb8dc6afa344b942ab42949101da1a328
Lars von Krüchten <lars.vonkruechten@web.de>
2021-05-16 14:58:41 +0200

    Added Post, Put, patch and delete methods to our APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -33,7 +33,6 @@ class HelloApiView(APIView):
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-
     def put(self, request, pk=None):
         """Handle updating an object"""
         return Response({'method': 'PUT'})
